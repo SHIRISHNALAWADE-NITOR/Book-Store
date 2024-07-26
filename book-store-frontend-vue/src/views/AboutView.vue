ab90b9f38b405fa3fcbde52776fd10be7b09cd3d
Mohit Jadhav <mohit.jadhav@nitorinfotech.com>
2024-07-26 11:48:37 +0530

    updatedVue

---
+++ book-store-frontend-vue/src/views/AboutView.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <HeaderComponent/>
+        Welcome to about View
+        
+    </div>
+</template>
+
+<script>
+
+import HeaderComponent from '@/components/HeaderComponent.vue';
+
+
+export default{
+    name : 'AboutView',
+    components : {
+        HeaderComponent,
+        
+    }
+}
+
+</script>
+
+<style>
+
+
+</style>
---
+++ book-store-frontend-vue/src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <CarouselComponent/>
+    <h1>Categories</h1>
+  </div>
+</template>
+
+<script>
+import CarouselComponent from '@/components/CarouselComponent.vue';
+
+export default {
+  name: 'HomeView',
+  components: {
+    CarouselComponent
+  }
+};
+</script>
+
+<style scoped>
+/* Scoped styles for HomeView component */
+</style>
