51a1e47132d069a3df97a766cb9cd73c72ba60ca
ShantanuKoyalkarNitor <shantanu.koyalkar@nitorinfotech.com>
2024-07-26 11:20:52 +0530

    admin-flow login logout

--- BookStoreBackEnd/BookStoreBackEnd/Migrations/20240723191427_migrations.cs
+++ BookStoreBackEnd/BookStoreBackEnd/Migrations/20240723191427_migrations.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace BookStoreBackEnd.Migrations
 {
     /// <inheritdoc />
-    public partial class TestMigration : Migration
+    public partial class migrations : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
