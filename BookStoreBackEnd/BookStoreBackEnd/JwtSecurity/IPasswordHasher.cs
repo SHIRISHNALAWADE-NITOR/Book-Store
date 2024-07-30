b68d0679c39955d260b0f0dd27a0d24ac3621430
Shirish Nalawade <shirish.nalawade@nitorinfotech.com>
2024-07-30 12:32:20 +0530

    FullChange30-07

--- BookStoreBackEnd/BookStoreBackEnd/JwtSecurity/IPasswordHasher.cs
+++
@@ -1,5 +0,0 @@
-﻿public interface IPasswordHasher
-{
-    string HashPassword(string password);
-    bool VerifyPassword(string hashedPassword, string password);
-}
--- BookStoreBackEnd/BookStoreBackEnd/Services/IOrderItemService.cs
+++
@@ -1,9 +0,0 @@
-﻿public interface IOrderItemService
-{
-    Task<IEnumerable<OrderItemDTO>> GetAllOrderItemsAsync();
-    Task<OrderItemDTO> GetOrderItemByIdAsync(int id);
-    Task<OrderItemDTO> AddOrderItemAsync(OrderItemDTO orderItemDto);
-    Task<OrderItemDTO> UpdateOrderItemAsync(int id, OrderItemDTO orderItemDto);
-    Task<bool> DeleteOrderItemAsync(int id);
-}
-
