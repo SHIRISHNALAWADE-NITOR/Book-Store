3ca66c62a2e94e55dca297b5ce73274b196453d0
Shirish Nalawade <shirish.nalawade@nitorinfotech.com>
2024-08-12 02:09:37 +0530

    JwtMiddlwareBranchAdded

--- BookStoreBackEnd/BookStoreBackEnd/JwtSecurity/ResetPasswordDTO.cs
+++
@@ -1,8 +0,0 @@
-﻿public class ResetPasswordDTO
-{
-    public string Email { get; set; }
-    public DateTime DateOfBirth { get; set; }
-    public string Token { get; set; }
-    public string? Otp { get; set; }
-    public string NewPassword { get; set; }
-}
